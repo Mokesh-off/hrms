83575b76a93c23362835c55f87441a3877f531a6
TringSwathi <43088797+TringSwathi@users.noreply.github.com>
2018-09-27 12:28:49 +0530

    Leaverequest added (#3)
    Change made: Leave request added

---
+++ src/components/Leave/LeaveRequest/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from '../../Header/Header'
+import Footer from '../../Footer/Footer'
+import SideNav from '../../SideNav/SideNav';
+import LeaveRequest from './LeaveRequest'
+
+class LeaveReq extends Component{
+  render(){
+    return(
+      <div>
+        <Header />
+        <SideNav />
+        <LeaveRequest/>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default LeaveReq
