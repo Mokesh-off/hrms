23ae24b7c1db5d468e28a61b372401b2d0186f55
Rupali Nilesh Karale <43088786+TringRupali@users.noreply.github.com>
2018-09-27 15:22:24 +0530

    leave Approvals (#5)
    Change made: Leave approval

--- src/components/LeaveApproval/leaveIndex.js
+++ src/components/LeaveApproval/leaveIndex.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import SideNav from '../SideNav/SideNav';
-//import LeaveRequests from '../LeaveApproval/LeaveRequests';
+import LeaveRequests from '../LeaveApproval/LeaveRequests';
 class Dashboard extends Component{
-
   render(){
-    if(JSON.parse(localStorage.getItem('currentUserId')) === null){
-      return <Redirect to='/' />
-    }
     return(
-
       <div>
-        {console.log(JSON.parse(localStorage.getItem('currentUserId')))}
         <Header />
-        <SideNav />    
+        <SideNav />
         <Footer />
+       <LeaveRequests/>
       </div>
     )
   }
--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import './SideNav.css';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+//import LeaveApprovals from './LeaveApproval'
 
 class SideNav extends Component{
 constructor(props){
   super(props)
   this.state={
-    employeeVisibility : ''
-  }
+  navFlag:'',
+  employeeVisibility : ''
+
+ }
+ this.navigation=this.navigation.bind(this)
 }
 
+navigation(e,link){
+  this.setState({navFlag:this.state.navFlag=link})
+  console.log(this.state.navFlag)
+}
+  
 componentWillMount(){
   var visibilityVar= JSON.parse(localStorage.getItem('currentUserRole'));
   (visibilityVar==='Employee')&&
@@ -17,12 +27,17 @@ componentWillMount(){
   }
 
   render(){
+   if (this.state.navFlag!=='') 
+        return <Redirect to={this.state.navFlag} />;
+
     return(
       <div className='sidenav'>
         <button>Home</button>
         <button>Leave</button>
+        <button>Leave Request</button>
+        {/* <Link to="/leaverequests"><button className="link">Leave Approval</button></Link> */}
+        <button className="link" onClick={e=>this.navigation(e,'/leaverequests')}>Leave Approval</button>
         <Link to='/leaverequest'><button>Leave Request</button></Link>
-        <button className={this.state.employeeVisibility}>Leave Approval</button>
         <button>My Leave</button>
         <button className={this.state.employeeVisibility}>Leave Records</button>
         <button>Leave Policy</button>
