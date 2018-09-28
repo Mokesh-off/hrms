afcb0ba499ac031b7f32cf1d3ad3df4d2334b33a
tringjanaswini <43532005+tringjanaswini@users.noreply.github.com>
2018-09-28 11:35:29 +0530

    Feature/leave policy (#10)
    Change made: leave policy

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,30 +1,41 @@
-import React, { Component } from 'react';
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import SideNav from '../SideNav/SideNav';
-//import LeaveRequests from '../LeaveApproval/LeaveRequests';
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import SideNav from "../SideNav/SideNav";
+import LeavePolicy from "../LeavePolicy/LeavePolicy";
+// import DummyComponent from "./DummyComponent";
+// import { Redirect } from "react-router-dom";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-class Dashboard extends Component{
-
-  render(){
-    if(JSON.parse(localStorage.getItem('currentUserId')) === null){
-      return <Redirect to='/' />
-    }
-    return(
-
-
-
-      <div>
-        {console.log(JSON.parse(localStorage.getItem('currentUserId')))}
-        <Header />
-        <SideNav />    
-        <Footer />
-      
-
-      </div>
-    )
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
+  state = {
+    clickComponent: null
+  };
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(e.currentTarget.id);
+    this.setState({ clickComponent: e.currentTarget.id });
+  }
+
+  render() {
+    if (JSON.parse(localStorage.getItem("currentUserId")) === null) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div>
+        {console.log(JSON.parse(localStorage.getItem("currentUserId")))}
+        <Header />
+        <SideNav onSubmit={this.onSubmit} />
+        {this.state.clickComponent !== null ? <LeavePolicy /> : null}
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default Dashboard
+export default Dashboard;
