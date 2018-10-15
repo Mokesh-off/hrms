686c233fd9f6a2483a53b53dd7b2b04e2d683703
Mokesh <mokesh.s@tringapps.com>
2018-10-15 18:16:51 +0530

    calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,73 +1,56 @@
 // import React...
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment'
  
 // ... and fullcalendar-reactwrapper.
 import FullCalendar from 'fullcalendar-reactwrapper';
+import '../../../node_modules/fullcalendar-reactwrapper/dist/css/fullcalendar.min.css'
 // fullcalendar-reactwrapper/dist/css/fullcalendar.min.css
+import './Calendar.css'
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    events:[
-                {
-                    title: 'All Day Event',
-                    start: '2017-05-01'
-                },
-                {
-                    title: 'Long Event',
-                    start: '2017-05-07',
-                    end: '2017-05-10'
-                },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: '2017-05-09T16:00:00'
-                },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: '2017-05-16T16:00:00'
-                },
-                {
-                    title: 'Conference',
-                    start: '2017-05-11',
-                    end: '2017-05-13'
-                },
-                {
-                    title: 'Meeting',
-                    start: '2017-05-12T10:30:00',
-                    end: '2017-05-12T12:30:00'
-                },
-                {
-                    title: 'Birthday Party',
-                    start: '2017-05-13T07:00:00'
-                },
-                {
-                    title: 'Click for Google',
-                    url: 'http://google.com/',
-                    start: '2017-05-28'
-                }
-            ],		
+    calendarData:[],		
     }
   }
- 
+   
   render() {
+
+    var calendarData=JSON.parse(localStorage.getItem('Data'))
+    calendarData=calendarData.leaveRequest
+    var j=0
+    console.log(calendarData)
+    calendarData.forEach(i => {
+      if(i.status === 'Approved'){
+        var obj = Object.assign ( {}, {title:i.EmpName,
+         EmpId:i.EmpId, EmpName:i.EmpName,
+         start:(i.FromDate).split("T")[0], end:(i.ToDate).split("T")[0]})
+        this.state.calendarData[this.state.calendarData.length]= obj
+        j++
+      }
+    });
+
     return (
-      <div id="example-component">
-        <FullCalendar
-             id = "your-custom-ID"
-         header = {{
-            left: 'prev,next today myCustomButton',
-            center: 'title',
-            right: 'month,basicWeek,basicDay'
-        }}
-         defaultDate={'2017-09-12'}
-        navLinks= {true} // can click day/week names to navigate views
-        editable= {true}
-        eventLimit= {true} // allow "more" link when too many events
-        events = {this.state.events}	
-    />
+      <div id='fullCalendarMainContainer'>
+        <div id='fullCalendarContainer'>
+          <div id="example-component">
+            <FullCalendar
+                id = "your-custom-ID"
+            header = {{
+                left: 'prev,next today myCustomButton',
+                center: 'title',
+                right: 'month,basicWeek,basicDay'
+            }}
+            defaultDate={'2017-09-12'}
+            navLinks= {true} // can click day/week names to navigate views
+            editable= {true}
+            eventLimit= {true} // allow "more" link when too many calendarData
+            events = {this.state.calendarData}	
+        />
+          </div>
+        </div>
       </div>
     );
   }
